4c0d2f341681a7dbbca7415270aa10c0c32a5e54
chellej80 <embarrequestrian@gmail.com>
2022-03-06 23:18:05 +0000

    created the select word variable under the run game function

--- run.py
+++ run.py
@@ -41,3 +41,63 @@ def play_again():
         run_game()
     else:
         print("Hope you enjoyed the game !. See you next time :)")
+
+# Define function to run the gamey
+def run_game():
+    # Set guess word to get_word function for a random word to be generated
+    word = select_word()
+    # Initiate an empty list for guessed letter
+    guessed_letters = []
+    # Initiate a counter for number of tries by the user
+    total_attempt_counter = 6
+    # Set inital guess to false
+    sucessfully_guessed = False
+    wrong_guess = 0
+    # Print an empty line
+    print()
+    # Initate a while loop and create decisions
+    # Also a create decisions for if user inputs a wrong entry
+    # Deduct attempts each user fails to guess incorrectly
+    while sucessfully_guessed is not True and total_attempt_counter > 0:
+        print('You have ' + str(total_attempt_counter) + ' attempts')
+        guessed_letter = input('Guess a letter: \n').lower()
+        # user inputs a letter
+        if len(guessed_letter) == 1:
+            if guessed_letter not in ALPHABETS:
+                print('You are yet to enter a letter. Check your entry, make sure you enter an alphabet not a number')
+            elif guessed_letter in guessed_letters:
+                print('You have already guessed that letter before.Try again!')
+            elif guessed_letter not in word:
+                print('Sorry, that letter is not part of the word')
+                guessed_letters.append(guessed_letter)
+                total_attempt_counter -= 1
+                wrong_guess += 1
+                hangman_pic(wrong_guess)
+            elif guessed_letter in word:
+                print('Super that letter is in the word')
+                guessed_letters.append(guessed_letter)
+                # nattempt_counter -=1
+                hangman_pic(wrong_guess)
+        else:
+            print('Please enter only one Letter per try')
+            total_attempt_counter -= 1
+        # print letter or dash under hangman pic
+        result = ''
+        if sucessfully_guessed is not True:
+            for letter in word:
+                if letter in guessed_letters:
+                    result += letter
+                else:
+                    result += '_'
+            print(result.capitalize())
+        if result == word:
+            print()
+            sucessfully_guessed = True
+            print("Well Done you guessed the right word :)")
+            print("-------------------------------------------")
+        elif total_attempt_counter == 0:
+            print()
+            print("-------------------------------------------")
+            print("Opps! You ran out of guesses, Hard Luck !!")
+            print("The correct word was: ", word.capitalize())
+            print("-------------------------------------------")
