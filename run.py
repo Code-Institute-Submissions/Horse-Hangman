1b2c85431fae5766c383581bcf0c4a9ffe484c47
chellej80 <embarrequestrian@gmail.com>
2022-03-06 23:08:00 +0000

    define a function that asks the user for their name and accepts letters only

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 import time
 
 ALPHABETS = ('abcdefghijklmnopqrstuvwxyz')
+
+# Function asking for user to input their name
+# Validate that user inputs a letter only
+def take_username_input():
+    name = input("Enter your name: \n")
+    if name.isalpha():
+        return name.capitalize()
+    else:
+        print('Please enter your name using letter only: \n')
+        return take_username_input()
